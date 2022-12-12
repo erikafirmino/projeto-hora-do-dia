887017e43844c3404ef1b31716c7e7651649c89a
Erika Machado <erikafirmino.ads@gmail.com>
2022-12-12 19:30:25 -0300

    att

--- script.js
+++ script.js
@@ -10,19 +10,19 @@ function carregar(){
     var ano = data.getFullYear();
 
     msg.innerHTML = `Hoje é <strong>${dia} / ${mes} / ${ano} </strong><br>
-                    Agora são ${hora}:${minutos}.`
+                    Agora são ${hora}:${minutos}`
     if(hora >= 0 && hora < 12){
         //bom dia
         img.src = 'manha1.png'
-        texto.innerHTML = `<strong> Bom Dia </strong>`;
+        texto.innerHTML = `<strong> Bom Dia! </strong>`;
         document.body.style.background = '#ffe26c'
     } else if(hora >= 12 && hora < 18) {
         img.src = 'tarde1.png';
-        texto.innerHTML = `<strong> Boa Tarde </strong>`;
+        texto.innerHTML = `<strong> Boa Tarde! </strong>`;
         document.body.style.background = '#e2a171'
     }else{
         img.src = 'noite1.png';
-        texto.innerHTML = `<strong> Boa Noite </strong>`;
+        texto.innerHTML = `<strong> Boa Noite! </strong>`;
         document.body.style.background = '#3978a91'
     }
 }
