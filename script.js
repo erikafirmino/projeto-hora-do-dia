5a6cb567b505b41a49bb2497ecf161682df9be7c
Erika Machado <erikafirmino.ads@gmail.com>
2022-12-12 18:51:37 -0300

    atualizacao

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ function carregar(){
     if(hora >= 0 && hora < 12){
         //bom dia
         img.src = 'manha1.png'
-        texto.innerHTML = `<strong> Bom Dia </strong>`
+        texto.innerHTML = `<strong> Bom Dia </strong>`;
+        document.body.style.background = '#ffe26c'
     } else if(hora >= 12 && hora < 18) {
         img.src = 'tarde1.png';
         texto.innerHTML = `<strong> Boa Tarde </strong>`;
+        document.body.style.background = '#e2a171'
     }else{
         img.src = 'noite1.png';
         texto.innerHTML = `<strong> Boa Noite </strong>`;
+        document.body.style.background = '#3978a91'
     }
 }
